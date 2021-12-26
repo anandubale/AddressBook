a655c9f22124a060b086bc3be005e57e96aa1f48
anandubale <au26199@gmail.com>
2021-12-26 11:20:43 +0530

    Added Uc1 program to create new contacts

---
+++ AddressBook.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookTrail
+{
+    class AddressBook
+    {
+        private List<Contact> List { get; set; } = new List<Contact>();
+        public void AddContact(Contact contact)
+        {
+            List.Add(contact);
+        }
+
+        public void DisplayAllContact()
+        {
+            foreach (var contact in List)
+            {
+                Console.WriteLine(contact.FirstName + contact.LastName + "\nAddress: " + contact.Address + "\nCity: " + contact.City + "\nState: " + contact.State + "\nZip Code: " + contact.ZipCode + "\nPhone Number: " + contact.PhoneNumber + "\nEmail: " + contact.Email);
+
+            }
+        }
+    }
+           
+}
